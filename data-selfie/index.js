fe7d3f007fae6e423640354b7708115960d7da17
Michelle <michellelverge@gmail.com>
2021-07-22 07:14:44 -0700

    updates server and cleans up html file

--- data-selfie/index.js
+++ data-selfie/index.js
@@ -1,18 +1,16 @@
 const { response } = require('express');
 const express = require('express');
+
 const app = express();
 app.listen(3000, () => console.log('listening on port 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const allData = [];
+
 app.post('/api', (req, res) => {
-  console.log(req.body);
   const data = req.body;
-  console.log(data.lat);
-  console.log(data.lon);
-  res.json({
-    status: 'success',
-    latitude: data.lat,
-    longitude: data.lon
-  });
+  allData.push(data);
+  res.json(allData);
+  console.log(allData);
 });
