674e6061a14d074e4e35fde55e4bde8b08c5103c
Michelle <michellelverge@gmail.com>
2021-07-22 07:30:10 -0700

    creates db and stores mock data

--- data-selfie/index.js
+++ data-selfie/index.js
@@ -1,16 +1,18 @@
-const { response } = require('express');
 const express = require('express');
+const Datastore = require('nedb');
 
 const app = express();
 app.listen(3000, () => console.log('listening on port 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-const allData = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
+database.insert({ name: 'Michelle', status: 'happy'});
 
 app.post('/api', (req, res) => {
   const data = req.body;
-  allData.push(data);
+  database.push(data);
   res.json(allData);
   console.log(allData);
 });
