a4427afec6cca26addf6b1ec5e997772c18ffa4a
Michelle <michellelverge@gmail.com>
2021-07-29 06:09:49 -0700

    moves js to own files and adds nav links

---
+++ data-selfie/public/logs/logs.js
@@ -0,0 +1,23 @@
+getData();
+      async function getData() {
+        const response = await fetch('/api');
+        const data = await response.json();
+
+        for (item of data) {
+          const root = document.createElement('div');
+          const mood = document.createElement('div');
+          const geo = document.createElement('div');
+          const date = document.createElement('div');
+          const image = document.createElement('img');
+
+          mood.textContent = `mood: ${item.mood}`;
+          geo.textContent = `${item.lat}&deg, ${item.lon}&deg`;
+          const dateString = new Date(item.timestamp).toLocaleString();
+          date.textContent = dateString;
+          image.src = item.image64;
+
+          root.append(mood, geo, date, image);
+          document.body.append(root);
+        }
+        console.log(data);
+      }
